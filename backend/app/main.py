1d0add92d6f88a947e21355fa988bbbfa87be687
ksajan <ksajan12@gmail.com>
2022-12-04 12:13:55 +0530

    updated files

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,17 +4,27 @@ import random
 import time
 
 import pandas as df
+import uvicorn
 from fastapi import FastAPI
-from selenium import webdriver
+from fastapi.middleware.cors import CORSMiddleware
+
+# from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-wd = webdriver.Chrome("Configs_Abis/chromedriver", options=chrome_options)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# wd = webdriver.Chrome(options=chrome_options)
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 now = datetime.datetime.now()
 minute = datetime.timedelta(days=0, seconds=5, microseconds=0)
 
@@ -44,9 +54,19 @@ def read_item(name: str):
 
 @app.get("/dune/{name}")
 def read_item(name: str):
-    wd.get("https://dune.com/queries/1620054")
-    time.sleep(20)
-    html = wd.page_source
-    s = df.read_html(html)
-    print(s)
-    return {"data": s[0]}
+    # wd.get("https://dune.com/queries/1620054")
+    time.sleep(2)
+    # html = wd.page_source
+    # s = df.read_html(html)
+    # print(s)
+    return {"data": [random.randint(1, 100) for _ in range(10)]}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8002,
+        reload=True,
+        debug=True,
+    )
