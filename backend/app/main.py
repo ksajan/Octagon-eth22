781cae61790b392c6c07e1e2559cd95285d761a0
ksajan <ksajan12@gmail.com>
2022-12-04 11:32:52 +0530

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,23 @@
 import datetime
 import math
 import random
-from typing import Union
+import time
 
-import numpy as np
+import pandas as df
 from fastapi import FastAPI
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+wd = webdriver.Chrome("Configs_Abis/chromedriver", options=chrome_options)
 app = FastAPI()
 
 now = datetime.datetime.now()
-minute = datetime.timedelta(days=0,seconds=5,microseconds=0)
+minute = datetime.timedelta(days=0, seconds=5, microseconds=0)
+
 
 @app.get("/")
 def read_root():
@@ -18,13 +26,27 @@ def read_root():
 
 @app.get("/widget/{name}")
 def read_item(name: str):
-    timestampList = [(now - (datetime.timedelta(days=0,seconds=5,microseconds=0) * i)).strftime("%H:%M:%S") for i in range(100)]
+    timestampList = [
+        (now - (datetime.timedelta(days=0, seconds=5, microseconds=0) * i)).strftime(
+            "%H:%M:%S"
+        )
+        for i in range(10)
+    ]
     data = {
         "name": name,
-        "type": 'Graph',
-        "value": '10%',
-        "data": [random.randint(1,100) for _ in range(100)],
-        "timestamp": timestampList
-          
+        "type": "Graph",
+        "value": "10%",
+        "data": [random.randint(1, 100) for _ in range(10)],
+        "timestamp": timestampList,
     }
     return {"data": data}
+
+
+@app.get("/dune/{name}")
+def read_item(name: str):
+    wd.get("https://dune.com/queries/1620054")
+    time.sleep(20)
+    html = wd.page_source
+    s = df.read_html(html)
+    print(s)
+    return {"data": s[0]}
